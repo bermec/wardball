1baee87a4a555c27d40472165e44e55f66e6d931
Roger Ward <rog@pynguins.com>
2017-11-04 16:29:15 +0000

    Add class to code

--- wardball/game.py
+++ wardball/game.py
@@ -1,18 +1,30 @@
 import sdl2.ext
-from .paths import IMAGES
+from .paths import PADDLE
+
+
+class Game:
+    window = sdl2.ext.Window("WardBall", size=(640, 480))
+
+    def init_game(self):
+        sdl2.ext.init()
+        self.window.show()
+
+    def render_paddle(self):
+        factory = sdl2.ext.SpriteFactory(sdl2.ext.SOFTWARE)
+        sprite = factory.from_image(PADDLE)
+
+        sprite_renderer = factory.create_sprite_render_system(self.window)
+        sprite_renderer.render(sprite)
+
+    def event_loop(self):
+        processor = sdl2.ext.TestEventProcessor()
+        processor.run(self.window)
 
 
 def launch_game():
-    sdl2.ext.init()
+    game = Game()
+    game.init_game()
 
-    window = sdl2.ext.Window("Hello World!", size=(640, 480))
-    window.show()
 
-    factory = sdl2.ext.SpriteFactory(sdl2.ext.SOFTWARE)
-    sprite = factory.from_image(IMAGES.get_path("paddle.png"))
 
-    sprite_renderer = factory.create_sprite_render_system(window)
-    sprite_renderer.render(sprite)
 
-    processor = sdl2.ext.TestEventProcessor()
-    processor.run(window)
--- wardball/paths.py
+++ wardball/paths.py
@@ -1,3 +1,5 @@
 import sdl2.ext
 
 IMAGES = sdl2.ext.Resources(__file__, "images")
+PADDLE = IMAGES.get_path("paddle.png")
+BALL = IMAGES.get_path("ball.png")
